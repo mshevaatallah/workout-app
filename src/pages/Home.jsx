7bac828f992cda3f036379e24000b9b0eb1e6728
sxhvxa <muhammadshax07@gmail.com>
2022-11-05 11:12:58 +0700

    add fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [workout, setWorkout] = useState(null);
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+      if (response.ok) {
+        setWorkout(json);
+      }
+    };
+    fetchWorkout();
+  }, []);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workout &&
+          workout.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
+    </div>
+  );
 }
 
 export default Home;

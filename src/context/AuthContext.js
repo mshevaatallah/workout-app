9a49a9d54d50fe0d556e2c00514d2bafff67870b
sxhvxa <muhammadshax07@gmail.com>
2022-11-11 19:44:33 +0700

    add context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -17,5 +17,9 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { user: null });
   console.log("AuthContext state :", state);
 
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
